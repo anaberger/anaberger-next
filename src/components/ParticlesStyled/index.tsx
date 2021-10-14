95022d82a08002258fb24431d8a487fa68f19452
anabergerr <ana.berger@lawbot.com.br>
2021-10-14 15:08:49 -0300

    height 100% particles]

--- src/components/ParticlesStyled/index.tsx
+++ src/components/ParticlesStyled/index.tsx
@@ -2,62 +2,70 @@ import Particles from "react-particles-js";
 
 export default function ParticlesStyled() {
   return (
-    <Particles
-      params={{
-        particles: {
-          number: {
-            value: 160,
-            density: {
-              enable: false,
-            },
-          },
-          color: {
-            value: ["#f72585"],
-          },
-
-          size: {
-            value: 3,
-            random: true,
-            anim: {
-              speed: 4,
-              size_min: 0.3,
-            },
-          },
-          line_linked: {
-            enable: false,
-          },
-          move: {
-            random: true,
-            speed: 1,
-            direction: "top",
-            out_mode: "out",
-          },
-        },
-        interactivity: {
-          events: {
-            onhover: {
-              enable: false,
-              mode: "bubble",
-            },
-            onclick: {
-              enable: true,
-              mode: "repulse",
-            },
-          },
-          modes: {
-            bubble: {
-              distance: 250,
-              duration: 2,
-              size: 0,
-              opacity: 0,
-            },
-            repulse: {
-              distance: 400,
-              duration: 4,
-            },
-          },
-        },
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
       }}
-    />
+    >
+      <Particles
+        height={window.outerHeight}
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            color: {
+              value: ["#f72585"],
+            },
+
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: "top",
+              out_mode: "out",
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: false,
+                mode: "bubble",
+              },
+              onclick: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import clsx from "clsx";
 
 import { Box, Card, Container, Grid, Typography } from "@material-ui/core";
 import Head from "next/head";
@@ -8,14 +9,19 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import NewLayout from "../components/NewLayout";
 import { animated } from "react-spring";
 import { use3dEffect } from "use-3d-effect";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const useStyles = makeStyles((theme) => ({
   title: {
     fontSize: 30,
     textAlign: "center",
-    marginTop: 20,
+    marginTop: 60,
     color: "#d100d1",
     marginBottom: 30,
+  },
+  aboutMe: {
+    textAlign: "center",
   },
   profession: {
     textAlign: "center",
@@ -39,17 +45,54 @@ const useStyles = makeStyles((theme) => ({
   img: {
     width: 200,
   },
+  active: {
+    transition: "200ms",
+    transform: "translateY(3rem)",
+  },
+  icons: {
+    padding: 2,
+  },
+  hide: {
+    display: "none",
+  },
+  buttonAlien: {
+    display: "block",
+    outline: "none",
+    transition: "200ms cubic-bezier(.28, .65, .73, .45)",
+    transitionDelay: "450ms",
+    zIndex: 21,
+    border: "none",
+    background: " none",
+    cursor: " pointer",
+  },
+  lightBeam: {
+    position: "absolute",
+    background:
+      "linear-gradient(to left, rgba(155, 130, 255, .3), rgba(185, 168, 255, .6), rgba(155, 130, 255, .3) )",
+    width: "100%",
+    height: "100%",
+    left: 0,
+    bottom: 0,
+    transform:
+      "translateY(3rem) matrix3d( .2, 0, 1, 0, 0, .882948, 0.428884, -.004, -0.913545, 0, 0.406737, 0, 0, 0, 0, 1 )",
+    boxShadow: "0 0 3rem rgb(255 120 242 / 60%)",
+    filter: "blur(1rem)",
+
+    zIndex: -1,
+    opacity: 0.5,
+  },
 }));
 
 export default function HomePage() {
   const classes = useStyles();
   const [isActive, setActive] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(true);
   const ref = useRef(null);
 
   const { style, ...mouseHandlers } = use3dEffect(ref);
 
-  const handleToggle = () => {
-    setActive(!isActive);
+  const handleOpen = () => {
+    setOpen(!open);
   };
 
   return (
@@ -62,60 +105,88 @@ export default function HomePage() {
       </Head>
 
       <NewLayout>
-        <Box position="relative">
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            margin: "0",
+            padding: "0",
+          }}
+        >
           <ParticlesStyled />
-          <Box
-            position="absolute"
-            top={0}
-            left={0}
-            bottom={0}
-            right={0}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
           >
-            <Box width="50%">
-              <Box display="flex" justifyContent="center">
-                <img
-                  src="/contact-alien.svg"
-                  alt="Foto de Ana Laura Berger"
-                  style={{ width: "200px" }}
-                  className={isActive ? "active" : null}
-                  onClick={handleToggle}
-                />
-              </Box>
+            <Box
+              position="absolute"
+              top={0}
+              left={0}
+              bottom={0}
+              right={0}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Box width="50%" marginTop={10}>
+                <Box display="flex" justifyContent="center" position="relative">
+                  <button
+                    className={clsx(classes.buttonAlien, {
+                      [classes.active]: open,
+                    })}
+                    onClick={handleOpen}
+                  >
+                    <img
+                      src="/contact-alien.svg"
+                      alt="Foto de Ana Laura Berger"
+                      style={{ width: "200px" }}
+                      className={clsx(open && classes.imgTop)}
+                    />
+                  </button>
+                  <div
+                    className={clsx(classes.lightBeam, open && classes.hide)}
+                  ></div>
+                </Box>
 
-              <Box
-                display="flex"
-                justifyContent="center"
-                marginTop={20}
-                flexDirection="column"
-              >
-                <animated.div
-                  ref={ref}
-                  style={{
-                    color: "white",
-                    padding: "2em",
-                    ...style,
-                  }}
-                  {...mouseHandlers}
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  flexDirection="column"
+                  width="80%"
+                  margin="auto"
                 >
-                  <Typography className={classes.title}>
-                    {" "}
-                    {`< Whos is Ana? />`}
+                  <animated.div
+                    ref={ref}
+                    style={{
+                      color: "white",
+
+                      ...style,
+                    }}
+                    {...mouseHandlers}
+                  >
+                    <Typography className={classes.title}>
+                      {" "}
+                      {`< Whos is Ana? />`}
+                    </Typography>
+                  </animated.div>
+                  <Typography className={classes.aboutMe}>
+                    is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy
+                    text ever since the 1500s, when an unknown printer took a
+                    galley of type and scrambled it to make a type specimen
+                    book. It has survived not only five centuries, but also the
+                    leap into electronic
                   </Typography>
-                </animated.div>
-                <Typography>
-                  is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry's standard dummy text ever
-                  since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book. It has survived
-                  not only five centuries, but also the leap into electronic
-                </Typography>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        </Box>
+          </div>
+        </div>
       </NewLayout>
     </>
   );
